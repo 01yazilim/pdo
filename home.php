db756715d5f17cc604d000ff0f7ac823d8ff8bae
01yazilim <37631041+01yazilim@users.noreply.github.com>
2018-03-21 22:51:05 +0300

    Create home.php

---
+++ home.php
@@ -0,0 +1,27 @@
+<?php 
+	require_once 'database-connect.php'; 
+
+		if (!isset($_SESSION["login"]) || !isset($_SESSION["eposta"])) {
+			header("Location:login.php");
+		}else{
+				$ip = $_SERVER['REMOTE_ADDR'];
+				$sorgu = $db->prepare('SELECT * FROM hesap WHERE eposta=?');
+				$sorgu->execute([$_SESSION['eposta']]);
+				$result = $sorgu->fetch(PDO::FETCH_ASSOC);
+				
+				if(!$result){
+					header('Location:login.php');
+					exit;
+				}else{
+					if($result['ip']!=$ip){
+						header('Location:login.php');
+						exit;
+					}else{
+						//GEREKLİ İŞLEMLER BURADA YAPILACAK
+						//GEREKLİ İŞLEMLER BURADA YAPILACAK
+						//GEREKLİ İŞLEMLER BURADA YAPILACAK
+					}
+				}
+				
+			}
+?>
